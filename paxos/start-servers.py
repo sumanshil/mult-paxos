2fb587a3887d2e3c7daa029c33c377a6b66f3c2a
Suman Chandra Shil <sshil@pinterest.com>
2021-12-18 14:46:26 -0800

    Initial commit for multi paxos

---
+++ paxos/start-servers.py
@@ -0,0 +1,33 @@
+import argparse
+import json
+import os.path
+import subprocess
+import sys
+
+
+def main(config: dict, config_path: str):
+    nodes = []
+    #config = {}
+    #config['nodes'] = {"localhost:5000"}
+    for s in config["nodes"]:
+        host, port = s.split(':')
+        nodes.append(subprocess.Popen([
+            sys.executable,
+            os.path.join(os.path.dirname(__file__), 'server.py'),
+            '--port',
+            port,
+            '--config',
+            config_path
+        ]))
+    for n in nodes:
+        n.wait()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Paxos node")
+    parser.add_argument("config", type=argparse.FileType(),
+                        help="JSON config file (see example-config.json)")
+    args = parser.parse_args()
+    main(json.load(args.config), args.config.name)
+
+
+
