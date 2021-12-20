b2e9e4924ed832d819012540be9b6cd911a380e5
Suman Chandra Shil <sshil@pinterest.com>
2021-12-19 21:47:25 -0800

    start Replica following 'Moderately Complex'

--- paxos/network.py
+++ paxos/network.py
@@ -4,16 +4,18 @@ import logging
 from typing import Optional
 
 
-def send(node: str, url: str, raw_message: dict) -> Optional[dict]:
+def send(node: str, url: str, raw_message: dict) -> dict | None:
     try:
-        response = requests.post(f"http://{node}{url}", json=raw_message)
+        response = requests.post(f"http://{node}{url}",
+                                 json=raw_message,
+                                 timeout=10)
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as exc:
         logging.getLogger("requests").warning(exc)
 
 
-def send_all(nodes: list[str], url: str, raw_message: dict) -> list[Optional[dict]]:
+def send_all(nodes: list[str], url: str, raw_message: dict) -> list[dict | None]:
     def send_one(node):
         return send(node, url, raw_message)
     with concurrent.futures.ThreadPoolExecutor() as executor:
